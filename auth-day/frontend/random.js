c621913f6c829957d97456f2b1397f11e9a89cc1
Anna <48394404+World-Cup@users.noreply.github.com>
2019-08-01 18:55:09 -0400

    commited

---
+++ auth-day/frontend/random.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class random extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default random
